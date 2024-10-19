4b68a9cd88c3e5f41f032e2bd7951a4d6fc142f7
code-with-prakhar <prakhar.saxena@outlook.com>
2024-10-19 13:49:37 +0530

    Update ReverseString.java

--- src/Strings/Day1ReverseString/ReverseString.java
+++ src/Strings/Day1ReverseString/ReverseString.java
@@ -1,7 +1,30 @@
 package Strings.Day1ReverseString;
 
-public class ReverseString {
+/**
+ * See Leetcode Link: {@link https://leetcode.com/problems/reverse-string/}
+ */
+class ReverseString {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "codewithprakhar";
+        System.out.println(reverseString(str));
+    }
+
+    public static String reverseString(String str) {
+        char[] charArray = str.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        while (start < end) {
+            swap(charArray, start, end);
+            start++;
+            end--;
+        }
+        return new String(charArray);
+    }
+
+    public static void swap(char[] charArray, int start, int end) {
+        char temp = charArray[start];
+        charArray[start] = charArray[end];
+        charArray[end] = temp;
     }
 }
