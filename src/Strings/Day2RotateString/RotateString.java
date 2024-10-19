6b5fce05567aa7c153954990ad10db7841f4421a
code-with-prakhar <prakhar.saxena@outlook.com>
2024-10-19 13:54:17 +0530

    Day 2 Rotate String

---
+++ src/Strings/Day2RotateString/RotateString.java
@@ -0,0 +1,36 @@
+package Strings.Day2RotateString;
+
+class RotateString {
+
+    public static void main(String[] args) {
+        String str1 = "abcde";
+        String str2 = "cdeab";
+        System.out.println(rotateString(str1, str2));
+    }
+
+    public static boolean rotateString(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        else {
+            for (int i = 0; i < s1.length(); i++) {
+                // checking character at ith index with
+                // first character of s2
+                if (s1.charAt(i) == s2.charAt(0)) {
+                    // checking prefix of s2 with suffix of
+                    // s1
+                    if (s1.substring(i).equals(s2.substring(
+                            0, s1.length() - i))) {
+                        // checking prefix of s1 with suffix
+                        // of s2
+                        if (s1.substring(0, i).equals(
+                                s2.substring(s1.length()
+                                        - i)))
+                            return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
